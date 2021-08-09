f233c2f28d109da325b1659d4395be90634f7fc6
Murilo Fregonesi Falleiros <murilo.fregonesi@sensedata.com.br>
2021-08-09 08:33:49 -0300

    Bootstrap

--- app.js
+++ app.js
@@ -3,26 +3,69 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 
 // App
 const app = express();
-const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log(`APP listening on PORT ${PORT}`)
-})
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
-})
+});
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-})
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground })
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground })
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+});
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log(`APP listening on PORT ${PORT}`)
+});
 
 // Database
 const DB_HOST = 'localhost';
--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,15 @@ const seedDB = async () => {
     await Campground.deleteMany({});
 
     for (let i=0; i<50; i++) {
-        let index = Math.floor(Math.random() * cities.length);
+        const index = Math.floor(Math.random() * cities.length);
+        const price = Math.floor(Math.random() * 20) + 10;
+
         const new_camp = new Campground({
             location: `${cities[index].city}, ${cities[index].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            price: price,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo optio, perferendis veniam fugiat vel, repudiandae libero dolor ex quos voluptatem incidunt iste quis atque repellat illum labore nesciunt suscipit esse.'
         });
         await new_camp.save();
     }

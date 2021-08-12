a8925393d3f3660141f3be05067180d764bd697b
Murilo Fregonesi Falleiros <murilo.fregonesi@sensedata.com.br>
2021-08-12 19:27:55 -0300

    Joi schemas validation

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
-const { wrapAsync } = require('./utils/ExpressError');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
 
 
 // App
@@ -19,6 +20,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -34,7 +46,7 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 });
 
-app.post('/campgrounds', catchAsync(async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`);
@@ -52,7 +64,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`${campground._id}`);
@@ -65,8 +77,14 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+})
+
 app.use((err, req, res, next) => {
-    res.send('Oh boy...');
+    const { statusCode=500 } = err;
+    if (!err.message) err.message = 'Error';
+    res.status(statusCode).render('error', { err });
 })
 
 const PORT = 3000;
